619fdbb3b97c0da1618b218ab987cda96c9c17ac
Oksana <10092@mail.ru>
2017-06-01 11:35:39 +0300

    com

---
+++ HMT_01/Task_01/MathOperation.cs
@@ -0,0 +1,250 @@
+﻿namespace Task_01
+{
+    using System;
+    /// <summary>
+    /// вычистительная логика программы по пункту а-к
+    /// </summary>
+    internal class MathOperation
+    {
+        private enum Vars
+        {
+            X, Y, X1, X2, X3, Y1, Y2, Y3
+        }
+
+        /// <summary>
+        /// пункт "а" - окружность
+        /// </summary>
+        public string ACircleOne()
+        {
+            float x = (float)Vars.X;
+            float y = (float)Vars.Y;
+            string v;
+            if (((x * x) + (y * y) <= 1) && (Math.Abs(x) <= 1 && Math.Abs(y) <= 1))
+            {
+                v = " принадлежит фигуре ";
+            }
+            else
+            {
+                v = " не принадлежит фигуре ";
+            }
+
+            return v;
+        }
+
+        /// <summary>
+        /// пункт "б" - окружность
+        /// </summary>
+        public string BCircleTwo()
+        {
+            string v;
+            float x = (float)Vars.X;
+            float y = (float)Vars.Y;
+
+            if (((x * x) + (y * y) <= 1) && (x <= 1 && x >= -1 && y <= 1 && y >= -1))
+            {
+                if (((x * x) + (y * y) >= 0.25) && (x >= 0.5 || (x <= -0.5 && y >= 0.5) || y <= -0.5))
+                {
+                    v = " принадлежит фигуре ";
+                }
+                else
+                {
+                    v = " не принадлежит фигуре ";
+                }
+
+                return v;
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// пункт "д" - ромб
+        /// </summary>
+        public string Rhomb()
+        {
+            // Разобьем на два треугольника и проверим принадлежность точки каждому из них
+            float x = (float)Vars.X;
+            float y = (float)Vars.Y;
+            float x1 = (float)Vars.X1;
+            float y1 = (float)Vars.Y1;
+            float x2 = (float)Vars.X2;
+            float y2 = (float)Vars.Y2;
+            float x3 = (float)Vars.X3;
+            float y3 = (float)Vars.Y3;
+            string v;
+
+            x1 = -0.5f;
+            x2 = 0;
+            x3 = 0.5f;
+            y1 = 0;
+            y2 = 1;
+            y3 = 0;
+
+            if (((x - x1) * (y1 - y2)) - ((y - y1) * (x1 - x2)) >= 0 &&
+                ((x - x2) * (y2 - y3)) - ((y - y2) * (x2 - x3)) >= 0 &&
+                    ((x - x3) * (y3 - y1)) - ((y - y3) * (x3 - x1)) >= 0)
+            {
+                v = " принадлежит фигуре ";
+            }
+            else if (
+                x1 == -0.5f && x2 == 0.5f && x3 == 0 &&
+                y1 == 0 && y2 == 0 && y3 == -1 &&
+                ((x - x1) * (y1 - y2)) - ((y - y1) * (x1 - x2)) >= 0 &&
+                ((x - x2) * (y2 - y3)) - ((y - y2) * (x2 - x3)) >= 0 &&
+                    ((x - x3) * (y3 - y1)) - ((y - y3) * (x3 - x1)) >= 0)
+            {
+                v = "  принадлежит фигуре ";
+            }
+            else
+            {
+                v = " не принадлежит фигуре ";
+            }
+
+            return v;
+        }
+
+        /// <summary>
+        /// пункт "треугольник и полуокружность"
+        /// </summary>
+        public string TriCir()
+        {
+            // Разобьем на две фигуры треугольник и полуокружность и проверим принадлежность точки каждой из них
+
+            // треуогольник
+            float x = (float)Vars.X;
+            float y = (float)Vars.Y;
+            string v;
+
+            float t1, t2, t3, n1, n2, n3, a, b, c;
+
+            t1 = 0;
+            t2 = 0;
+            t3 = -2;
+            n1 = 1;
+            n2 = -1;
+            n3 = 0;
+
+            a = ((t1 - x) * (n2 - n1)) - ((t2 - t1) * (n1 - y));
+            b = ((t2 - x) * (n3 - n2)) - ((t3 - t2) * (n2 - y));
+            c = ((t3 - x) * (n1 - n3)) - ((t1 - t3) * (n3 - y));
+
+            if ((a >= 0 && b >= 0 && c >= 0) || (a <= 0 && b <= 0 && c <= 0))
+            {
+                v = " принадлежит фигуре ";
+            }
+
+            // полуокружность
+            else if
+                ((x * x) + (y * y) <= 1 && x <= 1 && x >= 0 && Math.Abs(y) <= 1)
+            {
+                v = " принадлежит фигуре ";
+            }
+            else
+            {
+                v = " не принадлежит фигуре ";
+            }
+
+            return v;
+        }
+
+        /// <summary>
+        /// пункт "ж" - треугольник
+        /// </summary>
+        public string Triangle()
+        {
+            float x = (float)Vars.X;
+            float y = (float)Vars.Y;
+            float p1, p2, p3, q1, q2, q3, m, n, k;
+            string v;
+
+            p1 = 0;
+            p2 = 1.5f;
+            p3 = -1.5f;
+            q1 = 2;
+            q2 = -1;
+            q3 = -1;
+
+            m = ((p1 - x) * (q2 - q1)) - ((p2 - p1) * (q1 - y));
+            n = ((p2 - x) * (q3 - q2)) - ((p3 - p2) * (q2 - y));
+            k = ((p3 - x) * (q1 - q3)) - ((p1 - p3) * (q3 - y));
+
+            if ((m >= 0 && n >= 0 && k >= 0) || (m <= 0 && n <= 0 && k <= 0))
+            {
+                v = " принадлежит фигуре ";
+            }
+            else
+            {
+                v = " не принадлежит фигуре ";
+            }
+
+            return v;
+        }
+
+        /// <summary>
+        /// пункт "з" - многоугольник
+        /// </summary>
+        public string Polygon()
+        {
+            // многоугольник
+            // Разобьем на два треугольника и проверим принадлежность точки каждому из них
+            float x = (float)Vars.X;
+            float y = (float)Vars.Y;
+            float s1, s2, s3, w1, w2, w3;
+            string v;
+
+            s1 = -1;
+            s2 = -1.5f;
+            s3 = 0;
+            w1 = 1;
+            w2 = 0;
+            w3 = 0;
+
+            if (((x - s1) * (w1 - w2)) - ((y - w1) * (s1 - s2)) >= 0 &&
+                ((x - s2) * (w2 - w3)) - ((y - w2) * (s2 - s3)) >= 0 &&
+                    ((x - w3) * (w3 - w1)) - ((y - w3) * (s3 - s1)) >= 0)
+            {
+                v = " принадлежит фигуре ";
+            }
+            else if (
+                s1 == -1.5f && s2 == 1 && s3 == -2 &&
+                w1 == 0 && w2 == 0 && w3 == -2 &&
+                ((x - s1) * (w1 - w2)) - ((y - w1) * (s1 - s2)) >= 0 &&
+                ((x - s2) * (w2 - w3)) - ((y - w2) * (s2 - s3)) >= 0 &&
+                    ((x - s3) * (w3 - w1)) - ((y - w3) * (s3 - s1)) >= 0)
+            {
+                v = " принадлежит фигуре ";
+            }
+            else
+            {
+                v = " не принадлежит фигуре ";
+            }
+
+            return v;
+        }
+
+        /// <summary>
+        /// пункт "к" - бесконечная плоскасть
+        /// </summary>
+        public string InfPlane()
+        {
+            float x = (float)Vars.X;
+            float y = (float)Vars.Y;
+            string v;
+
+            if ((x >= -1) && (x <= 1) && (y > 0) && (y >= Math.Abs(x)))
+            {
+                v = " принадлежит фигуре ";
+            }
+            else if (x >= 1 && x <= -1 && y >= 1)
+            {
+                v = " принадлежит фигуре ";
+            }
+            else
+            {
+                v = " не принадлежит фигуре ";
+            }
+
+            return v;
+        }
+    }
+}
---
+++ HMT_01/Task_01/Program.cs
@@ -0,0 +1,118 @@
+﻿namespace Task_01
+{
+    using System;
+
+    /// <summary>
+    /// Написать консольное приложение, которое проверяет принадлежность точки
+    /// к заштрихованной области.
+    /// Пользователь вводит координаты точки (x;y) и выбирает букву графика (а-к)
+    /// В консоли должно высветиться сообщение: "Точка [(x;y)] принадлежит фигуре [г]"
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            float x, y;
+            Console.CursorVisible = false; // чтобы не было мигающего курсора
+            string str, ok;
+            ok = "y";
+            while (ok == "y" || ok == "н")
+            {
+                Console.WriteLine("Введите координаты точки x, y");
+                Console.WriteLine("x = ");
+                x = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("y = ");
+                y = Convert.ToSingle(Console.ReadLine());
+                while (ok == "y" || ok == "н")
+                {
+                    Console.WriteLine("Выберите график, для которого Вы хотите определить принадлежность точки: \n а, б, в, г, д, е, ж, з, и, к \n");
+                    str = Console.ReadLine();
+                    switch (str)
+                    {
+                        // окружность 1
+                        case "а":
+                            MathOperation newACircle = new MathOperation();
+                            Console.Write("Точка ({0}; {1}) {2} {3} \n", x, y, newACircle.ACircleOne(), str);
+                            ok = string.Empty;
+                            break;
+
+                        // окружность 2
+                        case "б":
+                            MathOperation newCircle = new MathOperation();
+                            Console.Write("Точка ({0}; {1}) {2} {3} \n", x, y, newCircle.BCircleTwo(), str);
+                            ok = string.Empty;
+                            break;
+
+                        // квадрат
+                        case "в":
+                            Console.Write("Точка ({0}; {1}) ", x, y);
+                            Console.WriteLine(x <= 1 && x >= -1 && y <= 1 && y >= -1 ? " принадлежит " : " не принадлежит ");
+                            Console.Write("графику {0} \n", str);
+                            ok = string.Empty;
+                            break;
+
+                        // правильный ромб
+                        case "г":
+                            Console.Write("Точка ({0};{1}) ", x, y);
+                            Console.WriteLine((Math.Abs(x + y) <= 1 && Math.Abs(x - y) <= 1) ? " принадлежит " : " не принадлежит ");
+                            Console.Write("графику {0} \n", str);
+                            ok = string.Empty;
+                            break;
+
+                        // ромб
+                        case "д":
+                            MathOperation newRhomb = new MathOperation();
+                            Console.Write("Точка ({0}; {1}) {2} {3} \n", x, y, newRhomb.Rhomb(), str);
+                            ok = string.Empty;
+                            break;
+
+                        // треугольник и полуокружность
+                        case "е":
+                            MathOperation newTriCir = new MathOperation();
+                            Console.Write("Точка ({0}; {1}) {2} {3} \n", x, y, newTriCir.TriCir(), str);
+                            ok = string.Empty;
+                            break;
+
+                        // треугольник
+                        case "ж":
+                            MathOperation newTriangle = new MathOperation();
+                            Console.Write("Точка ({0}; {1}) {2} {3} \n", x, y, newTriangle.Triangle(), str);
+                            ok = string.Empty;
+                            break;
+
+                        // 2 треугольника и прямоугольник. Рассмотрена модуль функции
+                        case "з":
+                            Console.Write("Точка ({0};{1}) ", x, y);
+                            Console.Write(x >= -1 && x <= 1 && y >= -2 && y <= 1 && y <= Math.Abs(x) ? " принадлежит " : " не принадлежит ");
+                            Console.Write("графику {0} \n", str);
+                            ok = string.Empty;
+                            break;
+
+                        // многоугольник
+                        case "и":
+                            MathOperation newPolygon = new MathOperation();
+                            Console.Write("Точка ({0}; {1}) {2} {3} \n", x, y, newPolygon.Polygon(), str);
+                            ok = string.Empty;
+                            break;
+
+                        // бесконечная плоскость
+                        case "к":
+                            MathOperation newInfPlane = new MathOperation();
+                            Console.Write("Точка ({0}; {1}) {2} {3} \n", x, y, newInfPlane.InfPlane(), str);
+                            ok = string.Empty;
+                            break;
+
+                        default:
+                            Console.WriteLine("Ошибка! Необходимо вводить: а, б, в, г, д, е, ж, з, и или к. \n");
+                            ok = "y";
+                            break;
+                    }
+                }
+
+                Console.WriteLine("Хотите продолжить: y?");
+
+                ok = Convert.ToString(Console.ReadLine());
+            }
+        }
+    }
+}
